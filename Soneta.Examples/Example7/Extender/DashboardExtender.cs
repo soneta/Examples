f358e996784622b9631a4821873fbaaea4e9fa37
RS2065 <robert.szatkowski@enova.pl>
2014-08-29 14:38:26 +0200

    Dołączenie przykładu 7 - Dashboard

---
+++ Soneta.Examples/Example7/Extender/DashboardExtender.cs
@@ -0,0 +1,93 @@
+﻿#define EXAMPLE7
+
+using System.Linq;
+using Soneta.Business;
+using Soneta.Business.App;
+using Soneta.Examples.Example7.Extender;
+using Soneta.Tools;
+
+#if EXAMPLE7
+[assembly: Worker(typeof(DashboardExtender))]
+#endif
+
+namespace Soneta.Examples.Example7.Extender {
+
+    public class DashboardExtender {
+        public int LiczbaDodatnia
+        {
+            get { return 12; }
+        }
+
+        public int LiczbaUjemna {
+            get { return -56; }
+        }
+
+        public int MaxObrot {
+            get { return (int)DaneObrot.Max(d => d.Obrót); }
+        }
+
+        public int MinObrot {
+            get { return (int)DaneObrot.Min(d => d.Obrót); }
+        }
+
+        public int MaxPopulacja {
+            get { return (int)DanePopulacja.Max(d => d.Populacja); }
+        }
+
+        public int MinPopulacja {
+            get { return (int)DanePopulacja.Min(d => d.Populacja); }
+        }
+
+        public Wiersz[] DaneObrot {
+            get {
+                return obroty();
+            }
+        }
+
+        public WierszPopulacja[] DanePopulacja {
+            get {
+                return populacja();
+            }
+        }
+
+        public class Wiersz {
+            public string Miasto { get; set; }
+            public string Towar { get; set; }
+            public decimal Obrót { get; set; }
+            public decimal Marża { get; set; }
+        }
+
+        public class WierszPopulacja {
+            public string Miasto { get; set; }
+            public double Populacja { get; set; }
+        }
+
+        private static Wiersz[] obroty() {
+            return new[] {
+                new Wiersz{ Miasto="Kraków", Towar="A", Obrót=123, Marża=10 },
+                new Wiersz{ Miasto="Kraków", Towar="B", Obrót=234, Marża=20 },
+                new Wiersz{ Miasto="Kraków", Towar="C", Obrót=345, Marża=30 },
+                new Wiersz{ Miasto="Kraków", Towar="D", Obrót=300, Marża=30 }
+			};
+        }
+
+        private static WierszPopulacja[] populacja() {
+            return new[] {
+                new WierszPopulacja{ Miasto="Kraków", Populacja=758 },
+                new WierszPopulacja{ Miasto="Lublin", Populacja=347 },
+                new WierszPopulacja{ Miasto="Warszawa", Populacja=1715 },
+                new WierszPopulacja{ Miasto="Poznań", Populacja=550 },
+                new WierszPopulacja{ Miasto="Gdańsk", Populacja=460 },
+                new WierszPopulacja{ Miasto="Szczecin", Populacja=408 },
+                new WierszPopulacja{ Miasto="Wrocław", Populacja=631 }
+			};
+        }
+
+        public bool IsVisible {
+            get {
+                return BusApplication.Instance.Is365 && CoreTools.Version.StartsWith("10.5");
+            }
+        }
+
+    }
+}
---
+++ Soneta.Examples/Example7/README.md
@@ -0,0 +1,12 @@
+###Example 7###
+-----------------------------------------------------------------------------------------------------
+
+Przykład pokazuje tworzenie elementu dashboard. 
+
+W wyniku zastosowania dodatku, w panelu użytkownika pojawią się dodatkowe element w dahshboard o nazwie 
+Soneta.Examples.Obroty i Soneta.Examples.Populacja. Z uwagi na zastosowanie nowych elementów w pageform.xml
+przykład będzie widoczny od wersji 10.5
+
+* Extender\DashboardExetnder.cs
+
+    Przykładowa klasa extender'a implementująca dane widoczne na dashboard.
